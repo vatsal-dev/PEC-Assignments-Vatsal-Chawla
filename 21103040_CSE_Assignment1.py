b87ae33cf2a73fa9a38eab58b973ed53e06f8df3
vatsal-dev <61816778+vatsal-dev@users.noreply.github.com>
2022-01-08 16:49:58 +0530

    Update 21103040_CSE_Assignment1.py

--- 21103040_CSE_Assignment1.py
+++ 21103040_CSE_Assignment1.py
@@ -1,8 +1,25 @@
+#Ques 1: Find average of three numbers by taking inputs from user
+  
+#Define variables to take inputs
 a = int(input("What is the first number?"))
 b = int(input("What is the second number?"))
 c = int(input("What is the third number?"))
-
+  
+#Define variable to print average
 average = (a+b+c)/3
-average = str(average)
+
+#Print the formula for finding average
 print ("Formula to calculate average = sum of the numbers/3")
-print ("Average of the three numbers is " + average)
+
+#Print result
+print ("Average of the three numbers is " + str(average))
+
+#Output: 
+#What is the first number?3
+#What is the second number?3
+#What is the third number?3
+#Formula to calculate average = sum of the numbers/3
+#Average of the three numbers is 3.0
+
+
+#Ques 2: Write a python program to compute a person's income tax.
