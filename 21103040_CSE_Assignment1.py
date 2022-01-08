81357b71d0e6272d49a0c620f3b9a206ff58f051
vatsal-dev <61816778+vatsal-dev@users.noreply.github.com>
2022-01-08 16:38:43 +0530

    Add files via upload

---
+++ 21103040_CSE_Assignment1.py
@@ -0,0 +1,8 @@
+a = int(input("What is the first number?"))
+b = int(input("What is the second number?"))
+c = int(input("What is the third number?"))
+
+average = (a+b+c)/3
+average = str(average)
+print ("Formula to calculate average = sum of the numbers/3")
+print ("Average of the three numbers is " + average)
