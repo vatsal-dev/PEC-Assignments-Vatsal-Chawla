db0abb98eea7973a4d1792225cb67eb5c82fa96c
vatsal-dev <61816778+vatsal-dev@users.noreply.github.com>
2022-01-14 10:27:53 +0530

    Output File for Assignment 1 Uploaded

---
+++ Output_Assignment1.py
@@ -0,0 +1,57 @@
+#Answer 1: 
+#What is the first number?3
+#What is the second number?3
+#What is the third number?3
+#Formula to calculate average = sum of the numbers/3
+#Average of the three numbers is 3.0
+
+#Answer 2:
+Put your gross income($): 70000.92
+#Put number of dependents: 2
+#Your income tax is 10800.184000000001
+
+# Question 3 : To store different data type values into a list.
+
+Enter the name of the Student: Nikita Sharma
+Enter the Student Id: 21103002
+Enter the gender of the Student(F, M, U): F
+Enter the Student's course name: Introduction to Computing
+Enter the Student's CGPA: 10
+['Name', 'SID', 'Gender', 'Course name', 'CGPA']
+
+ The list of the Student information:
+  ['Nikita Sharma', 21103002, 'F', 'Introduction to Computing', 10.0]
+
+
+# Question 4 : Make a list to enter marks of 5 students into a list and display them in a sorted manner.
+
+Enter the marks of the Student 1: 96
+Enter the marks of the Student 2: 89
+Enter the marks of the Student 3: 93
+Enter the marks of the Student 4: 100
+Enter the marks of the Student 5: 85
+
+ List of the Student Marks:
+  [96, 89, 93, 100, 85]
+
+ Sorted Student List (increasing order):
+[85, 89, 93, 96, 100]
+
+
+"""
+Question 5:
+(a) Print a specified list after removing 4th element i.e. Black.
+(b) Remove Black and Pink from the list and replace it with Purple.
+"""
+
+ (a)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+ List after removing Black Color:
+  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+ (b)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+ List after replacing Black and Pink with Purple Color:
+  ['Red', 'Green', 'White', 'Purple', 'Yellow']
