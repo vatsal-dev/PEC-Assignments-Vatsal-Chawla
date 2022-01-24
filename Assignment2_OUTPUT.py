8c688828959103969039adf08050f4a0917e1a28
vatsal-dev <61816778+vatsal-dev@users.noreply.github.com>
2022-01-24 15:13:09 +0530

    Assignment2_OUTPUT Uploaded.

---
+++ Assignment2_OUTPUT.py
@@ -0,0 +1,55 @@
+Output 1:
+ 
+Part a: Length of the string =  35
+Part b: Here's the reverse order =
+ egaugnal evitisnes esac a si nohtyP
+Here's the new variable =  a case sensitive  
+Part d: Here's the asked replacement =  Python is a object oriented language
+Part e: here's the index of 'a' = 10
+Part f: Here's the string without white spaces: Pythonisacasesensitivelanguage
+
+
+--------------------------
+Output 2:
+Enter your SID: 2110XXX
+Enter your dept.:XYZ
+Enter your CGPA:9.9
+Hey, ABC Here!
+My SID is 2110XXX
+I am from XYZ department and my CGPA is 9.9
+
+---------------------------
+Output 3:
+Part a: a&b =  8
+Part b: a|b =  58
+Part c: a^b =  50
+Part d: Left shift a =  224
+Part d: Left shift b =  40
+Part e: Right shift a =  14
+Part e: Right shift b =  0
+
+---------------------------
+Output 4:
+Enter first number:
+3
+Enter second number:
+4
+Enter third number:
+2
+The largest number is 4.0
+
+-----------------------------
+Output 5:
+Please type a statement:
+hey my name is vatsal
+Yes
+
+-----------------------------
+Output 6:
+Enter first side:
+4
+Enter second side:
+5
+Enter third side:
+3
+Yes
