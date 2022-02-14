21149d390a1566f2dc6d2e4ddb82d08e392eab3e
vatsal-dev <61816778+vatsal-dev@users.noreply.github.com>
2022-02-14 13:48:19 +0530

    Create Assignment3_OUTPUT.py

---
+++ Assignment3_OUTPUT.py
@@ -0,0 +1,93 @@
+Ans1
+Enter the statement here:hey hello wow hey my name is hey
+{'hello': 1, 'hey': 3, 'is': 1, 'my': 1, 'name': 1, 'wow': 1}
+
+---------------------
+Ans 2
+Please enter Day- 28
+Please enter Month- 2
+Please enter Year- 2022
+Next Date is: 1/3/2022
+
+---------------------
+Ans 3
+Enter the list of numbers (separated with space):
+ 3 4 5 6 7 8 9
+[(3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81)]
+
+---------------------
+Ans 4
+Enter Grade Point - 5
+Your grade is 'C' and Below Average Performance
+
+---------------------
+Ans 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+   
+---------------------
+Ans 6: 
+
+Type Y to enter student details, otherwise type N: Y
+Enter the name:ABC    
+Enter the SID:123
+Type Y to enter student details, otherwise type N: Y
+Enter the name:JKL
+Enter the SID:890
+Type Y to enter student details, otherwise type N: Y
+Enter the name:QWE
+Enter the SID:456
+Type Y to enter student details, otherwise type N: Y
+Enter the name:VBN
+Enter the SID:678
+Type Y to enter student details, otherwise type N: N
+Program Input Mode ended.
+Ans a. Details of students -
+SID = 123, Name = ABC
+SID = 890, Name = JKL
+SID = 456, Name = QWE
+SID = 678, Name = VBN
+
+Ans b
+value sorted dictionary is ['ABC', 'JKL', 'QWE', 'VBN']
+
+Ans c
+Keys sorted dictionary is [123, 456, 678, 890]
+
+Ans D
+Please enter the student's SID whose detail you need- 456
+Name of the required student is QWE
+
+-----------------------
+Question 7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 10  
+Fibonnaci sequence with 10 terms
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+Average of total 10 terms of sequence is 8.8
+END
+
+--------------------------
+Question 8
+Part a
+set with elements not common in both is {1, 3, 5, 6, 8}
+Part b
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+Part C
+Set of elements that is only present in two set is {1, 2, 4, 5}
+Part d
+set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3 {10, 7}
