7520f237e505838d9567bc0a557517878e9975b1
vatsal-dev <61816778+vatsal-dev@users.noreply.github.com>
2022-02-27 12:15:03 +0530

    Create Assignment4_OUTPUT.py

---
+++ Assignment4_OUTPUT.py
@@ -0,0 +1,93 @@
+-------------
+Ans 1 -------------
+
+Enter the number of disks3
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+-------------
+Ans 2 -------------
+
+Enter the number: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+-------------
+Ans 3 -------------
+
+Enter first integer:10
+Enter second integer:2
+
+(Quotient, Remainder) is (5, 0)
+
+PART A Function is callable or not:  False
+
+PART B
+One/more digit is zero.
+
+PART C
+List after adding new values is [5, 0, 4, 5, 6]
+List after filtering out values greater than 4 is [0, 4]       
+
+PART D: convert to set
+{0, 4}
+
+PART E: make set immutable
+frozenset({0, 4})
+
+PART D: find max value and find its hash
+The maximum value from the set is  4
+Hash value of the max value is  4
+
+-------------
+Ans 4 -------------
+
+Making an object...
+Student Class created.
+The name of the student is Sample_Name and Roll Number is 123
+Function end...
+Program ended.
+Destructor called
+
+-------------
+Ans 5 -------------
+
+Ans 5
+Details of Employees:
+1 Mehak 40000
+2 Ashok 50000
+3 Viren 60000
+------
+
+PART A
+1 Mehak 70000
+
+PART B - refer to code as no output had to be printed for this part.
+
+-------------
+Ans 6 -------------
+
+Enter the word from friend 1:geale 
+Enter the word from friend 2:eagle
+ :)
+
+ :)
+
+ :)
+
+ :)
+
+
+Press N if any of the above result was :( , otherwise press Y  ---- Y
+
+
+Does the word 2 make sense? Y/n ---- Y
+Friendship is true
